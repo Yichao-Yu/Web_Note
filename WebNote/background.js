681e555af8ea087dfeac7b891aa3ff04334935b4
Yichao-Yu <yichaoyu@Danlis-MacBook-Pro.local>
2013-09-12 22:22:00 -0400

    fix context menu id naming issue

--- WebNote/background.js
+++ WebNote/background.js
@@ -4,7 +4,10 @@
 
 // The onClicked callback function.
 function onClickHandler(info, tab) {
-	switch (info.menuItemId) {
+	var itemId = info.menuItemId;
+	var context = itemId.split('_', 1)[0];
+	
+	switch (context) {
 		case 'selection':
 			if (info.selectionText.length > 0) {
 				console.log(JSON.stringify(info.selectionText));
@@ -29,8 +32,8 @@ chrome.runtime.onInstalled.addListener(function (object) {
 	for (var i = 0; i< contexts.length; i++) {
 		var context =  contexts[i];
 		var id = chrome.contextMenus.create({"title": "Add to Note", "contexts":[context], "id": context + "_context"});
-		chrome.contextMenus.create({"title": "Add to Recent notes...", "contexts":[context], "parentId": "context_" + context, "id": context + "_recent_notes"});
-		chrome.contextMenus.create({"title": "Open a new Note", "contexts":[context], "parentId": "context_" + context, "id": context + "_new_note"});
+		chrome.contextMenus.create({"title": "Add to Recent notes...", "contexts":[context], "parentId": context + "_context", "id": context + "_recent_notes"});
+		chrome.contextMenus.create({"title": "Open a new Note", "contexts":[context], "parentId": context + "_context", "id": context + "_new_note"});
 		// mock up the exsting recent notes
 		chrome.contextMenus.create({"title": "Note_1", "contexts":[context], "parentId": context + "_recent_notes", "id": context + "_recent_note_1"});
 		chrome.contextMenus.create({"title": "Note_2", "contexts":[context], "parentId": context + "_recent_notes", "id": context + "_recent_note_2"});
